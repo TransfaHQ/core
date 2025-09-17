7c433c0a6f7e66cce42d76f37697229bc26259ac
corentin <corentinalcoy@gmail.com>
2025-09-18 00:17:37 +0100

    Add nestjs project

--- scripts/tigerbeetle/start.sh
+++ scripts/tigerbeetle/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./tigerbeetle start --addresses=3000 --development ./data/0_0.tigerbeetle
+./tigerbeetle start --addresses=6066 --development ./data/0_0.tigerbeetle
