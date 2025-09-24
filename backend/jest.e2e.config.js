04ea6fdb753a3ab972c23552f6d3ef370ae90ca1
corentin <corentinalcoy@gmail.com>
2025-09-24 02:47:02 +0100

    - Fix jest config to support uuid

--- backend/jest.e2e.config.js
+++ backend/jest.e2e.config.js
@@ -15,12 +15,9 @@ module.exports = {
     '^@libs/(.*)$': '<rootDir>/src/libs/$1',
     '^@modules/(.*)$': '<rootDir>/src/modules/$1',
     '^@src/(.*)$': '<rootDir>/src/$1',
-    '^uuid$': '<rootDir>/../node_modules/uuid/dist-node',
   },
   testTimeout: 30000,
   setupFilesAfterEnv: [],
   globalTeardown: '<rootDir>/src/jest-e2e-global-teardown.ts',
-  transformIgnorePatterns: [
-    '/node_modules/(?!uuid)/', // transform uuid even though it's in node_modules
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!uuid)/'],
 };
