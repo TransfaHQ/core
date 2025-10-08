c6fe5ac2da2628dff85a28e3c0d3f86277f9d505
corentin <corentinalcoy@gmail.com>
2025-10-08 23:28:37 +0100

    Add effectivateAt column

--- backend/src/database/migrations/1759954902967-AddEffectivateDateToTransactionTable.ts
+++ backend/src/database/migrations/1759954902967-AddEffectivateDateToTransactionTable.ts
@@ -3,11 +3,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddEffectivateDateToTransactionTable1759954902967 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            alter table ledger_transactions add column effective_at timestamptz not null default now()
+            alter table ledger_transactions add column effective_at timestamptz not null default now();
+            CREATE INDEX idx_ledger_transactions_effective_at ON ledger_transactions(effective_at);
         `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`alter table ledger_transactions drop column effective_at`);
+    await queryRunner.query(`
+        drop index if exists idx_ledger_transactions_effective_at;
+        alter table ledger_transactions drop column effective_at;
+    `);
   }
 }
